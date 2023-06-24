62c3d1c55cf8867f02c84624ae77b81e36e09219
Brandon Gompert <brandongompert@gmail.com>
2023-06-24 12:14:23 -0400

    feat: add profile schema with migration files

---
+++ drizzle/0002_powerful_thing.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `profiles` (
+	`id` serial AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	`name` varchar(256),
+	`user_id` int);
--- src/db/schema/profiles.ts
+++ src/db/schema/profiles.ts
@@ -1,12 +1,13 @@
-import { mysqlTable, varchar, serial } from "drizzle-orm/mysql-core"
+import { mysqlTable, varchar, serial, int } from "drizzle-orm/mysql-core"
 import { users } from "./users"
 import { relations } from "drizzle-orm"
 
 export const profiles = mysqlTable("profiles", {
   id: serial("id").primaryKey().autoincrement(),
   name: varchar("name", { length: 256 }),
+  userId: int("user_id"),
 })
 
 export const profilesRelations = relations(profiles, ({ one }) => ({
-  user: one(users, { fields: [profiles.id], references: [users.id] }),
+  user: one(users, { fields: [profiles.userId], references: [users.id] }),
 }))
